b44ee8e27a2be9e405900c25ffe354ee2ef3b0ee
Zak <zakiabdessemed35@gmail.com>
2024-10-14 19:38:52 +0100

    Fixing bug

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,9 @@
 import { getEventsClient } from '@/actions/event'
 import { NextResponse } from 'next/server'
 
+export const dynamic = "force-dynamic";
+
+
 export async function GET() {
   try {
     const data = await getEventsClient()
