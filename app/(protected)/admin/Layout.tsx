deb3c745cb8619f0759a334cb45b586187b78c31
zakiabdess <zakiabdessemed35@gmail.com>
2024-10-15 01:10:09 +0100

    Deployemnt

--- app/(protected)/admin/Layout.tsx
+++ app/(protected)/admin/Layout.tsx
@@ -1,28 +1,32 @@
 'use client'
-import { Toaster } from '@/components/ui/sonner'
 import React from 'react'
 import Sidebar from '../_components/Sidebar'
 import Navigation from '../_components/Navigation'
-import { ToastContainer } from 'react-toastify'
+import { auth } from '@/auth'
+import { SessionProvider } from 'next-auth/react'
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <div className="bg-gray-100 scrollbar scrollbar-w-3 scrollbar-thumb-rounded-[0.25rem] scrollbar-track-slate-200 scrollbar-thumb-gray-400 min-h-[100vh]">
-      <Sidebar />
-      <div className="flex-grow p-4 sm:ml-64">
-        <div className="h-full p-4 rounded-lg ">
-          {/* <NavbarDashboard />*/}
+  const session = await auth() // Suspicious, possibly causing the issue
 
-          <main>
-            <Navigation />
-            <div className="p-4">{children}</div>
-          </main>
+  return (
+    <SessionProvider session={session}>
+      <div className="bg-gray-100 scrollbar scrollbar-w-3 scrollbar-thumb-rounded-[0.25rem] scrollbar-track-slate-200 scrollbar-thumb-gray-400 min-h-[100vh]">
+        <Sidebar />
+        <div className="flex-grow p-4 sm:ml-64">
+          <div className="h-full p-4 rounded-lg ">
+            {/* <NavbarDashboard />*/}
+
+            <main>
+              <Navigation />
+              <div className="p-4">{children}</div>
+            </main>
+          </div>
         </div>
       </div>
-    </div>
+    </SessionProvider>
   )
 }
