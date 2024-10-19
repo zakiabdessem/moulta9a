32a142b96bbc383c83f6bb49437486b47eb0bf15
zakiabdess <zakiabdessemed35@gmail.com>
2024-10-19 18:23:21 +0100

    fixing bugs

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
       <a href="/">LOGO</a>
       <ul className="flex justify-between items-center p-4 rounded-xl w-3/6 max-xl:hidden">
         <li>
-          <Link className="text-md font-bold" href="#events">
+          <Link className="text-md font-bold" href="/events">
             Events
           </Link>
         </li>
